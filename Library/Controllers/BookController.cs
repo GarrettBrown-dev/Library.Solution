fc98a55ad07e3f64e0b515e9ebd20ecc168c2a6b
Garrett <gman9mm@live.com>
2020-10-19 15:31:02 -0700

    Fix page. Login not working properly. Controller refactored, Book view no longer needs login. Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- Library/Controllers/BookController.cs
+++ Library/Controllers/BookController.cs
@@ -1,84 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
 using Library.Models;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using System.Linq;
 
 namespace Library.Controllers
 {
-  [Authorize]
   public class BooksController : Controller
   {
     private readonly LibraryContext _db;
-    private readonly UserManager<ApplicationUser> _userManager;
 
-    public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    public BooksController(LibraryContext db)
     {
-      _userManager = userManager;
       _db = db;
     }
 
-    public async Task<ActionResult> Index()
+    public ActionResult Index()
     {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      var userBooks = _db.Books.Where(entry => entry.User.Id == currentUser.Id).ToList();
-      return View(userBooks);
+      List<Book> model = _db.Books.ToList();
+      return View(model);
     }
 
     public ActionResult Create()
     {
-      ViewBag.CatalogId = new SelectList(_db.Catalogs, "CatalogId", "Name");
       return View();
     }
 
     [HttpPost]
-    public async Task<ActionResult> Create(Book book, int CatalogId)
+    public ActionResult Create(Book book)
     {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      book.User = currentUser;
       _db.Books.Add(book);
-      if (CatalogId != 0)
-      {
-        _db.AuthorBookCatalog.Add(new AuthorBookCatalog() { CatalogId = CatalogId, BookId = book.BookId });
-      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
     public ActionResult Details(int id)
     {
       var thisBook = _db.Books
-          .Include(book => book.JoinEntries)
-          .ThenInclude(join => join.Catalog)
-          .FirstOrDefault(book => book.BookId == id);
+        .Include(book => book.JoinEntries)
+        .ThenInclude(join => join.Author)
+        .FirstOrDefault(book => book.BookId == id);
       return View(thisBook);
     }
-
     public ActionResult Edit(int id)
     {
       var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
-      ViewBag.CatalogId = new SelectList(_db.Catalogs, "CatalogId", "Name");
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "AuthorName");
       return View(thisBook);
     }
 
     [HttpPost]
-    public ActionResult Edit(Book book, int CatalogId)
+    public ActionResult Edit(Book book)
     {
-      if (CatalogId != 0)
-      {
-        _db.AuthorBookCatalog.Add(new AuthorBookCatalog() { CatalogId = CatalogId, BookId = book.BookId });
-      }
       _db.Entry(book).State = EntityState.Modified;
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Details", new { id = book.BookId });
     }
-
     public ActionResult Delete(int id)
     {
       var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
@@ -93,29 +74,29 @@ namespace Library.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    public ActionResult AddCategory(int id)
-    {
-      var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
-      ViewBag.CatalogId = new SelectList(_db.Catalogs, "CatalogId", "Name");
-      return View(thisBook);
-    }
     [HttpPost]
-    public ActionResult AddCategory(Book book, int CatalogId)
-    {
-      if (CatalogId != 0)
-      {
-        _db.AuthorBookCatalog.Add(new AuthorBookCatalog() { CatalogId = CatalogId, BookId = book.BookId });
-      }
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
-    [HttpPost]
-    public ActionResult DeleteCategory(int joinId)
+    public ActionResult DeleteAuthor(int joinId, int bookId)
     {
       var joinEntry = _db.AuthorBookCatalog.FirstOrDefault(entry => entry.AuthorBookCatalogId == joinId);
       _db.AuthorBookCatalog.Remove(joinEntry);
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Details", new { id = bookId });
+    }
+    public ActionResult AddAuthor(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "AuthorName");
+      return View(thisBook);
+    }
+    [HttpPost]
+    public ActionResult AddAuthor(Book book, int AuthorId)
+    {
+      if (AuthorId != 0)
+      {
+        _db.AuthorBookCatalog.Add(new AuthorBookCatalog() { AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = book.BookId });
     }
   }
 }
